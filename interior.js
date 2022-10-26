01038cac6efbf90c6e94df9e697a0a01e7933404
Omoleiruafemi <106076157+Omoleiruafemi@users.noreply.github.com>
2022-10-26 02:57:18 -0700

    first commit

---
+++ interior.js
@@ -0,0 +1,25 @@
+// console.log('hello')
+let bars =  document.querySelector('.bar')
+let side = document.querySelector('.mySideNav');
+let closemenu = document.querySelector('.close')
+let interior = document.querySelector('.interior')
+
+
+// bars.addEventListener('click', function(){
+
+// })
+console.log(closemenu)
+bars.onclick = function () {
+    side.style.display = 'block';
+    
+
+}
+
+closemenu.onclick = function() {
+    side.style.display = 'none';
+    // console.log('helloo')
+}
+interior.onclick = function() {
+    // side.style.display = 'none';
+    // console.log('helloo')
+}
